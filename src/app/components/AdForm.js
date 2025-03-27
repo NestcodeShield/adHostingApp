550fb618027af6e1752e03f0a27b0694d479cdae
NestcodeShield <chaoshorizon2000@gmail.com>
2025-03-27 22:08:44 +0300

    0.0.2

--- src/app/components/AdForm.js
+++ src/app/components/AdForm.js
@@ -4,19 +4,51 @@ import Link from "next/link";
 const AdForm = () => {
   return (
    <div>
-    <form className="flex flex-col bg-slate-100 p-8 gap-10">
+    <form className="flex flex-col bg-slate-100 p-8 gap-10 w-200 border-gray-300 border">
           <label className="flex flex-col gap-2">
-            Название объявления
-            <input type="text" className="focus:outline-none bg-white p-2" placeholder="До 15 символов"></input>
+            <input type="text" className="focus:outline-none bg-white p-2" placeholder="Укажите заголовок"/>
           </label>
-          <label>Категория
-          <select id="category-select" className="focus:outline-none">
-            <option></option>
-            <option>Недвижимость</option>
-            <option>Транспорт</option>
+            <select className="focus:outline-none bg-white p-2">
+              <option>Выберите населённый пункт</option>
+              <option>Недвижимость</option>
+              <option>Транспорт</option>
+            </select>
+            <textarea
+            className="focus:outline-none bg-white p-2"
+            placeholder="Введите описание"
+          />
+          <select
+            className="focus:outline-none bg-white p-2"
+          >
+            <option value="">Выберите время действия объявления</option>
+            <option value="7">7 дней</option>
+            <option value="14">14 дней</option>
+            <option value="30">30 дней</option>
           </select>
-            
-             </label>
+          
+          <input
+            className="focus:outline-none bg-white p-2"
+            type="number"
+            placeholder="Введите цену"
+          />
+
+          <select
+          className="focus:outline-none bg-white p-2"
+          >
+          <option>Выберите населённый пункт</option>
+            <option>
+              BUDVA
+            </option>
+          </select>
+
+          <input
+            placeholder="Выбрать изображение"
+            type="file"
+          />
+
+          <button className="dynamic-button" type="submit">
+            Опубликовать объявление
+          </button>
     </form>
    </div>
   )
